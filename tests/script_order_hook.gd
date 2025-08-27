fc569be9d33a35af3a3bac162a6ce313f67063f4
tomsterBG <tomi4443321@gmail.com>
2025-08-27 16:14:18 +0300

    Test dependency ordering

--- tests/script_order_hook.gd
+++ tests/script_order_hook.gd
@@ -8,42 +8,80 @@
 extends GutHookScript
 
 
-#region variables
-var test_prefix = "res://tests/test_"
-var test_suffix = ".gd"
+#region constants
+const IS_DEEP = true
+#region constants
 
-var test_order: Array[String] = [
-	# Globals:
-	"helpers",
-	# Components:
-	"health",
-	"health_plus",
-]
+
+#region variables
+var path_prefix = "res://tests/test_"
+var path_suffix = ".gd"
+
+var test_scripts: Dictionary[StringName, Array] = {
+	# name = [dependencies],
+	# globals:
+	helpers = [],
+	# components:
+	health = [&"helpers"],
+	health_plus = [&"health", &"helpers"],
+}
 #endregion variables
 
 
 #region methods
-func get_test_paths(tests: Array[String]) -> Array[String]:
+func get_ordered_tests() -> Array[StringName]:
+	var ordered_tests: Array[StringName]
+	
+	for test: StringName in test_scripts.keys():
+		var dependencies := test_scripts[test]
+		if dependencies.size() == 0:
+			ordered_tests.push_front(test)
+			continue
+		
+		var idx_of_last_dependency: int = -1
+		for dependency in dependencies:
+			if ordered_tests.find(dependency) > idx_of_last_dependency:
+				idx_of_last_dependency = ordered_tests.find(dependency)
+		ordered_tests.insert(idx_of_last_dependency+1, test)
+	
+	print("ordered_tests = ", ordered_tests)
+	return ordered_tests
+
+func are_tests_ordered_correctly(ordered_tests: Array[StringName]) -> bool:
+	var tests := ordered_tests.duplicate(IS_DEEP)
+	tests.reverse()
+	
 	for idx in range(tests.size()):
-		tests[idx] = test_prefix + tests[idx] + test_suffix
+		var test_name: StringName = tests[idx]
+		var dependencies: Array = test_scripts[test_name]
+		for dependency in dependencies:
+			var dependency_idx: int = tests.find(dependency)
+			if dependency_idx < idx:
+				return false
+	return true
+
+func get_test_paths(tests: Array[StringName]) -> Array[StringName]:
+	for idx in range(tests.size()):
+		tests[idx] = path_prefix + tests[idx] + path_suffix
 	return tests
 #endregion methods
 
 
 #region virtual methods
 func run():
-	var ordered_tests = get_test_paths(test_order)
-	
-	print("Pre-run hook is re-ordering the tests.")
+	var ordered_tests := get_ordered_tests()
+	if are_tests_ordered_correctly(ordered_tests):
+		print("Tests are in the correct order.\n")
+	else:
+		push_error("Tests are in the wrong order.")
+	ordered_tests = get_test_paths(ordered_tests)
 	
 	for test_script in gut.get_test_collector().scripts:
 		if ordered_tests.has(test_script.path): continue
 		ordered_tests.append(test_script.path)
-		push_warning(str(test_script.path) + " is not added to pre-run order.")
+		push_error(str(test_script.path) + " is not added to pre-run order.")
 	
 	gut.get_test_collector().clear()
-	for test_path in ordered_tests:
-		gut.add_script(test_path)
-	
-	print("Hook has finished re-ordering tests.\n")
+	for test in ordered_tests:
+		gut.add_script(test)
 #endregion virtual methods
