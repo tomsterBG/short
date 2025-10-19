4586e5d3d7c73d55d3ea60808a4e66d9ab160e4f
tomsterBG <tomi4443321@gmail.com>
2025-10-19 14:54:12 +0300

    Remove get_ from Math geometry functions

--- addons/short/classes/math.gd
+++ addons/short/classes/math.gd
@@ -2,13 +2,14 @@
 # - https://www.omnicalculator.com
 # TODO:
 # - geometry
-# 	- get_rectangle_area(side_x, side_y)
-# 	- get_rectangle_perimeter(side_x, side_y)
-# 	- get_rectangle_diagonal(side_x, side_y)
-# 	- get_sphere_volume(radius)
-# 	- get_square_area(side_length)
-# 	- get_square_perimeter(side_length)
-# 	- get_square_diagonal(side_length)
+# 	- rectangle_area(side_x, side_y)
+# 	- rectangle_perimeter(side_x, side_y)
+# 	- rectangle_diagonal(side_x, side_y)
+# 	- right_triangle_area(side_x, side_y)
+# 	- triangle_perimeter(a, b, c)
+# 	- square_area(side_length)
+# 	- square_perimeter(side_length)
+# 	- square_diagonal(side_length)
 # IDEAS:
 # - class or class_name Matrix to more easily define what a matrix is
 # - add_matrix(matrix_a, matrix_b)
@@ -25,31 +26,31 @@
 #region methods
 	#region geometry
 ## Returns the area of a circle.
-static func get_circle_area(radius: float) -> float:
+static func circle_area(radius: float) -> float:
 	return PI * pow(radius, 2)
 
 ## Returns the circumference of a circle.
-static func get_circle_circumference(radius: float) -> float:
+static func circle_circumference(radius: float) -> float:
 	return TAU * radius
 
 ## Returns the volume of a cylinder.
-static func get_cylinder_volume(radius: float, height: float) -> float:
-	return get_circle_area(radius) * height
+static func cylinder_volume(radius: float, height: float) -> float:
+	return circle_area(radius) * height
 
 ## Returns the area of a triangle, given base and height.
-static func get_triangle_area_from_base_and_height(base: float, height: float) -> float:
+static func triangle_area_from_base_and_height(base: float, height: float) -> float:
 	return 0.5 * base * height
 
 ## Returns the area of a triangle, given three sides.
-static func get_triangle_area_from_three_sides(a: float, b: float, c: float) -> float:
+static func triangle_area_from_three_sides(a: float, b: float, c: float) -> float:
 	return 0.25 * sqrt( (a + b + c) * (-a + b + c) * (a - b + c) * (a + b - c) )
 
 ## Returns the area of a triangle, given two sides and [param angle_between_rad] them in [code]radians[/code].
-static func get_triangle_area_from_two_sides_and_angle_between(a: float, b: float, angle_between_rad: float) -> float:
+static func triangle_area_from_two_sides_and_angle_between(a: float, b: float, angle_between_rad: float) -> float:
 	return 0.5 * a * b * sin(angle_between_rad)
 
 ## Returns the area of a triangle, given a side and two adjacent angles in [code]radians[/code].
-static func get_triangle_area_from_side_and_adjacent_angles(side_between: float, angle_a_rad: float, angle_b_rad: float) -> float:
+static func triangle_area_from_side_and_adjacent_angles(side_between: float, angle_a_rad: float, angle_b_rad: float) -> float:
 	return pow(side_between, 2) * sin(angle_a_rad) * sin(angle_b_rad) / (2.0 * sin(angle_a_rad + angle_b_rad))
 	#endregion geometry
 
--- tests/test_math.gd
+++ tests/test_math.gd
@@ -7,20 +7,20 @@ const ERROR_INTERVAL := 0.000_001
 
 
 #region tests
-func test_get_circle_area():
-	assert_almost_eq(Math.get_circle_area(6.0), 113.097_335, ERROR_INTERVAL)
+func test_circle_area():
+	assert_almost_eq(Math.circle_area(6.0), 113.097_335, ERROR_INTERVAL)
 
-func test_get_circle_circumference():
-	assert_almost_eq(Math.get_circle_circumference(7.0), 43.982_297, ERROR_INTERVAL)
+func test_circle_circumference():
+	assert_almost_eq(Math.circle_circumference(7.0), 43.982_297, ERROR_INTERVAL)
 
-func test_get_cylinder_volume():
-	assert_almost_eq(Math.get_cylinder_volume(4.0, 7.0), 351.858_377, ERROR_INTERVAL)
+func test_cylinder_volume():
+	assert_almost_eq(Math.cylinder_volume(4.0, 7.0), 351.858_377, ERROR_INTERVAL)
 
-func test_get_triangle_area():
-	assert_eq(Math.get_triangle_area_from_base_and_height(5.0, 5.0), 12.5)
-	assert_almost_eq(Math.get_triangle_area_from_three_sides(5.0, 7.0, 11.090_076), 12.500_002, ERROR_INTERVAL)
-	assert_almost_eq(Math.get_triangle_area_from_two_sides_and_angle_between(5.0, 7.0, deg_to_rad(45.585)), 12.500_066, ERROR_INTERVAL)
-	assert_almost_eq(Math.get_triangle_area_from_side_and_adjacent_angles(5.0, deg_to_rad(51.0), deg_to_rad(63.0)), 9.474_653, ERROR_INTERVAL)
+func test_triangle_area():
+	assert_eq(Math.triangle_area_from_base_and_height(5.0, 5.0), 12.5)
+	assert_almost_eq(Math.triangle_area_from_three_sides(5.0, 7.0, 11.090_076), 12.500_002, ERROR_INTERVAL)
+	assert_almost_eq(Math.triangle_area_from_two_sides_and_angle_between(5.0, 7.0, deg_to_rad(45.585)), 12.500_066, ERROR_INTERVAL)
+	assert_almost_eq(Math.triangle_area_from_side_and_adjacent_angles(5.0, deg_to_rad(51.0), deg_to_rad(63.0)), 9.474_653, ERROR_INTERVAL)
 
 func test_get_projected_vector():
 	assert_eq(Math.get_projected_vector(Vector2(3, 0), Vector2(3, 0)), Vector2(3, 0), "No change.")
