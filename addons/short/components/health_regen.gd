2e3cb0ba8f8feaa27ba705aabd6c84542b900df7
tomsterBG <tomi4443321@gmail.com>
2025-12-18 08:59:21 +0200

    Refactor HealthRegen: remove Time dependency
    HealthRegen is no longer dependent on TIme because that was breaking a bunch of logic, such as what happens if you pause the game engine.
    It was also making the component borderline untestable.

--- addons/short/components/health_regen.gd
+++ addons/short/components/health_regen.gd
@@ -2,7 +2,7 @@
 # IDEAS:
 # BAD IDEAS:
 # - Signals: is_regenerating_changed.
-# - Add paused signal which does not emit if regen_pause_for == 0.
+# - Add paused signal which does not emit if regen_pause_length == 0.
 
 ## @experimental: This class is immature.
 ## Regeneration for [Health].
@@ -41,27 +41,32 @@ class SimulateRegenResult:
 @export var regen_in__process := true
 
 ## Heal this much [member Health.health] each second.
-@export var regen_per_second := 0.0
+@export_custom(PROPERTY_HINT_NONE, "suffix:hp/s") var regen_per_second := 0.0
 
-## Pause regeneration for this many seconds.
-@export var regen_pause_for := 0.0
+@export_subgroup("Pause", "regen_pause")
 
 ## Toggles if regen pauses when [member health] is damaged.
 @export var regen_pause_when_damaged := true
 
-## Timestamp of when regen was paused, using [method Time.get_ticks_msec].
-var regen_paused_at := -1_000_000_000_000_000
+## Pause regeneration for this many seconds.
+@export_custom(PROPERTY_HINT_NONE, "suffix:sec") var regen_pause_length := 0.0
+
+## Seconds until regen pause ends.
+var regen_pause_time_left := 0.0: set = set_regen_pause_time_left
 #endregion variables
 
 
 #region setters
-func set_health(value: Health):
+func set_health(value: Health) -> void:
+	if health:
+		health.health_changed.disconnect(_on_health_changed)
 	if value:
 		value.health_changed.connect(_on_health_changed)
-	elif health:
-		health.health_changed.disconnect(_on_health_changed)
 	health = value
 	if Engine.is_editor_hint(): update_configuration_warnings()
+
+func set_regen_pause_time_left(value: float) -> void:
+	regen_pause_time_left = clamp(value, 0.0, regen_pause_length)
 #endregion setters
 
 
@@ -71,25 +76,21 @@ func get_seconds_for_full_regen() -> float:
 	if !regen_enabled or regen_per_second == 0.0: return INF
 	return health.max_health / regen_per_second
 
-## How many seconds until pause ends?
-func get_pause_time_left() -> float:
-	return max(0.0, regen_pause_for - Convert.msec_to_sec(get_time_since_pause()))
-
-## How many msec since pausing regen?
+## How many seconds since pausing regen?
 func get_time_since_pause() -> float:
-	return Time.get_ticks_msec() - regen_paused_at
+	return regen_pause_length - regen_pause_time_left
 #endregion getters
 
 
 #region methods
 ## Pause regeneration.
 func pause_regen() -> void:
-	if regen_pause_for == 0.0: return
-	regen_paused_at = Time.get_ticks_msec()
+	if regen_pause_length == 0.0: return
+	regen_pause_time_left = regen_pause_length
 
-## Is time since pause more than [member regen_pause_for]?
+## Syntax sugar for [member regen_pause_time_left] [code]> 0.0[/code].
 func is_regen_paused() -> bool:
-	return get_time_since_pause() < Convert.sec_to_msec(regen_pause_for)
+	return regen_pause_time_left > 0.0
 
 ## Simulate regen for [param delta] seconds.
 func simulate_regen(delta: float) -> SimulateRegenResult:
@@ -99,8 +100,8 @@ func simulate_regen(delta: float) -> SimulateRegenResult:
 		return result
 	
 	result.success = true
-	result.heal_result = health.heal(regen_per_second * (delta - get_pause_time_left()))
-	regen_paused_at -= int(Convert.sec_to_msec(delta))
+	result.heal_result = health.heal(regen_per_second * (delta - regen_pause_time_left))
+	regen_pause_time_left -= delta
 	return result
 #endregion methods
 
--- tests/accurate/test_health_regen.gd
+++ tests/accurate/test_health_regen.gd
@@ -34,19 +34,24 @@ func test_initial_values():
 	assert_eq(health_regen.regen_in_editor, false, "Regen in editor is off.")
 	assert_eq(health_regen.regen_in__process, true, "Regen in _process is on.")
 	assert_eq(health_regen.regen_per_second, 0.0, "Regen per second is 0.")
-	assert_eq(health_regen.regen_pause_for, 0.0, "Regen pause for 0.")
-	assert_eq(health_regen.regen_paused_at, -1_000_000_000_000_000, "Regen paused at -1_000_000_000_000_000.")
+	assert_eq(health_regen.regen_pause_length, 0.0, "Regen pause length is 0.")
 	assert_eq(health_regen.regen_pause_when_damaged, true, "Regen pauses when health is damaged.")
+	assert_eq(health_regen.regen_pause_time_left, 0.0, "No regen pause time.")
 
 func test_initial_method_values():
 	assert_eq(health_regen.get_seconds_for_full_regen(), INF, "Won't regen.")
 	assert_false(health_regen.is_regen_paused(), "Regen is not paused.")
-	assert_eq(health_regen.get_pause_time_left(), 0.0, "No pause time.")
 
-func test_regen_paused_at():
-	health_regen.regen_pause_for = 1.0
+func test_value_clamping():
+	health_regen.regen_pause_time_left = -1.0
+	assert_eq(health_regen.regen_pause_time_left, 0.0, "Pause_time_left can't be negative.")
+	health_regen.regen_pause_time_left = 2.0
+	assert_eq(health_regen.regen_pause_time_left, health_regen.regen_pause_length, "Can't set pause_time_left above pause_length.")
+
+func test_regen_pause_time_left():
+	health_regen.regen_pause_length = 1.0
 	health_regen.pause_regen()
-	assert_eq(health_regen.regen_paused_at, Time.get_ticks_msec(), "Paused now.")
+	assert_eq(health_regen.regen_pause_time_left, health_regen.regen_pause_length, "Paused for 1 second.")
 
 func test_get_seconds_for_full_regen():
 	health_regen.regen_per_second = health.max_health / 2.0
@@ -56,18 +61,18 @@ func test_get_seconds_for_full_regen():
 
 func test_get_time_since_pause():
 	health_regen.regen_in__process = false
-	health_regen.regen_pause_for = 1.0
+	health_regen.regen_pause_length = 1.0
 	health_regen.pause_regen()
 	assert_eq(health_regen.get_time_since_pause(), 0.0, "No time since pause.")
 	health_regen.regen_per_second = 1.0
 	health_regen.simulate_regen(1.0)
-	assert_eq(health_regen.get_time_since_pause(), Convert.sec_to_msec(1.0), "A second since pause.")
+	assert_eq(health_regen.get_time_since_pause(), 1.0, "A second since pause.")
 
 func test_is_regen_paused():
 	health_regen.pause_regen()
 	assert_false(health_regen.is_regen_paused(), "Regen is not paused.")
 	health_regen.regen_per_second = 1.0
-	health_regen.regen_pause_for = 1.0
+	health_regen.regen_pause_length = 1.0
 	health_regen.pause_regen()
 	assert_true(health_regen.is_regen_paused(), "Regen is paused.")
 	health_regen.simulate_regen(2.0)
@@ -94,26 +99,26 @@ func test_regen_enabled():
 	assert_null(result.heal_result, "No heal result.")
 	assert_eq(health.health, 20.0, "Health didn't change.")
 
-func test_regen_pause_for():
+func test_regen_pause_length():
 	health_regen.regen_in__process = false
 	health_regen.regen_per_second = 60.0
-	health_regen.regen_pause_for = 2.0
+	health_regen.regen_pause_length = 2.0
 	health.damage(60)
-	assert_eq(health_regen.get_pause_time_left(), 2.0, "Regen is paused for 2 seconds.")
+	assert_eq(health_regen.regen_pause_time_left, 2.0, "Regen is paused for 2 seconds.")
 	health_regen.simulate_regen(1.5)
 	assert_eq(health.health, 40.0, "Health didn't change.")
-	assert_eq(health_regen.get_pause_time_left(), 0.5, "Regen is paused for 0.5 seconds.")
+	assert_eq(health_regen.regen_pause_time_left, 0.5, "Regen is paused for 0.5 seconds.")
 	health_regen.simulate_regen(1.0)
 	assert_eq(health.health, 70.0, "Half regenerated to 70.")
-	assert_eq(health_regen.get_pause_time_left(), 0.0, "Regen is not paused since 0.5 seconds.")
+	assert_eq(health_regen.regen_pause_time_left, 0.0, "Regen is not paused since 0.5 seconds.")
 
 func test_regen_pause_when_damaged():
 	health_regen.regen_in__process = false
 	health_regen.regen_per_second = 40.0
-	health_regen.regen_pause_for = 100.0
+	health_regen.regen_pause_length = 100.0
 	health_regen.regen_pause_when_damaged = false
 	health.damage(40)
-	assert_eq(health_regen.get_pause_time_left(), 0.0, "Regen is not paused.")
+	assert_eq(health_regen.regen_pause_time_left, 0.0, "Regen is not paused.")
 	health_regen.simulate_regen(1.0)
 	assert_eq(health.health, health.max_health, "Fully regenerated.")
 
