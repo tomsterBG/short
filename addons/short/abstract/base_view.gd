b45466af65d4c6eb73190247e307953d857c00c6
tomsterBG <tomi4443321@gmail.com>
2026-06-14 12:27:13 +0300

    Remove BaseView
    It was very weird and app-specific. Just useless.

--- addons/short/abstract/base_view.gd
+++
@@ -1,8 +0,0 @@
-## @experimental: This class could change. Untested.
-## A UI view that needs to be refreshed.
-
-@abstract class_name BaseView extends Control
-
-
-## Call this to refresh the view.
-@abstract func refresh() -> void
