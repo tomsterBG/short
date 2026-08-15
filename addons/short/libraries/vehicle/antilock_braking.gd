9d62a6feb53b03e07b564e176f71111081575d7f
tomsterBG <tomi4443321@gmail.com>
2026-08-15 11:58:22 +0300

    Update ABS

--- addons/short/libraries/vehicle/antilock_braking.gd
+++ addons/short/libraries/vehicle/antilock_braking.gd
@@ -22,8 +22,8 @@
 # IDEAS:
 # - Add slip ratio / friction coefficient curves for different surfaces. May be better off in a template.
 # - Input brake ratio and output brake ratio that can't be more than the input.
-# - sensor_info: Array[SensorInfo] - contains any sensor readings necessary.
 # - hydraulic_channels() -> int - returns how many channels the ABS system has.
+# - signal cycled() - Emitted when this ABS goes through a cycle.
 
 ## @experimental: This class could change.
 ## Anti-lock braking system (ABS).
@@ -31,25 +31,90 @@
 ## Applicable to any vehicle. See also [url=https://en.wikipedia.org/wiki/Anti-lock_braking_system]Anti-lock braking system[/url] and [VehicleLib].
 ##[br][br][b]Note:[/b] Can't be called ABS because that confuses it with [method @GlobalScope.abs].
 
+@tool
 class_name AntilockBraking extends Node
+
+
+#region enums
+## Defines different ABS cycle types to be used for each sensor.
+enum ABSCycle {
+	PASS, ## The sensor is currently doing nothing, letting brake pressure pass through.
+	DECREASE, ## The sensor is currently decreasing brake pressure.
+	HOLD, ## The sensor is currently holding brake pressure.
+	INCREASE, ## The sensor is currently increasing brake pressure.
+}
+#endregion enums
 
 
 #region variables
 @export_group("ABS")
 
-# IDEAS: Clamp to 0 and 1 with a small step like 0.01 or 0.001 in export. Also clamp with set_target_slip.
 ## Target slip ratio. See [method VehicleLib.wheel_slip_ratiof] for more info on slip ratio.
 ##[br][br][b]Note:[/b] Tarmac usually has best wheel grip at [code]-0.12[/code] slip. Gravel at [code]-1.0[/code].
 ##[br][br][b]Note:[/b] To simulate modern ABS surface modes, you may want to change this value to the best slip ratio for each surface.
-@export var target_slip := -0.12
+@export_range(-1.0, 0.0, 0.001) var target_slip := -0.12: set = set_target_slip
 
 ## How often the ABS tries to adjust in [code]cycles per second (cps, or Hz)[/code]. Older systems have less cycles, like [code]2-8[/code] while modern systems have more cycles, like [code]15[/code], some even claim to go up to [code]100[/code].
 @export var cycles_per_second := 10
+
+## Sensor readings from all sensors.
+var sensor_info: Array[SensorInfo] = []
+
+## Seconds until abs cycle ends.
+var cycle_time_left: float = 0.0: set = set_cycle_time_left
 #endregion variables
+
+
+#region setters
+func set_target_slip(value: float) -> void:
+	target_slip = clampf(value, -1.0, 0.0)
+
+func set_cycle_time_left(value: float) -> void:
+	cycle_time_left = fposmod(value, cycle_time())
+#endregion setters
 
 
 #region methods
 ## Time between each cycle in [code]seconds[/code].
 func cycle_time() -> float:
 	return Convert.hz_to_delta_sec(cycles_per_second)
+
+## Simulate ABS for [param delta] seconds.
+func simulate_abs(delta: float) -> void:
+	if cycle_time_left - delta < 0.0: # NOTE: Doesn't account for multiple cycles per simulation step.
+		for info: SensorInfo in sensor_info:
+			info.simulate_cycle()
+	cycle_time_left -= delta
 #endregion methods
+
+
+#region classes
+## Contains all necessary sensor data. Older systems have less sensors while newer systems have one per wheel.
+class SensorInfo:
+	## Average slip ratio of the wheel(s) this sensor is looking at. Set this value.
+	var slip_ratio: float = 0.0: set = set_slip_ratio
+	## Average input brake ratio of the wheel(s) this sensor is looking at. Set this value.
+	var input_brake_ratio: float = 0.0: set = set_input_brake_ratio
+	## ABS cycle last chosen by this sensor.
+	var abs_cycle: ABSCycle = ABSCycle.PASS
+	## Average output brake ratio of the wheel(s) this sensor is controlling.
+	var output_brake_ratio: float = 0.0: set = set_output_brake_ratio
+	
+	func set_slip_ratio(value: float) -> void:
+		slip_ratio = clampf(value, -1.0, 0.0)
+	
+	func set_input_brake_ratio(value: float) -> void:
+		input_brake_ratio = clampf(value, 0.0, 1.0)
+	
+	func set_output_brake_ratio(value: float) -> void:
+		output_brake_ratio = clampf(value, 0.0, 1.0)
+	
+	##@experimental: Untested.
+	## Simulate a sensor cycle.
+	func simulate_cycle() -> void:
+		#if slip_ratio > target_slip:
+			#abs_cycle = ABSCycle.DECREASE
+		#elif slip_ratio < target_slip:
+			#abs_cycle = ABSCycle.PASS
+		pass
+#endregion classes
--- tests/accurate/test_antilock_braking.gd
+++ tests/accurate/test_antilock_braking.gd
@@ -3,6 +3,7 @@ extends GutTest
 
 #region variables
 var antilock_braking: AntilockBraking
+var sensor_info: AntilockBraking.SensorInfo
 #endregion variables
 
 
@@ -11,6 +12,7 @@ func before_each():
 	antilock_braking = AntilockBraking.new()
 	add_child_autofree(antilock_braking)
 	watch_signals(antilock_braking)
+	sensor_info = AntilockBraking.SensorInfo.new()
 #endregion virtual
 
 
@@ -18,7 +20,46 @@ func before_each():
 func test_initial_values():
 	assert_eq(antilock_braking.target_slip, -0.12, "Target slip is 12%.")
 	assert_eq(antilock_braking.cycles_per_second, 10, "Cycles per second are 10.")
+	assert_eq(antilock_braking.sensor_info, [], "Sensor info is empty.")
+	assert_eq(antilock_braking.cycle_time_left, 0.0, "Abs cycle time left is 0.")
 
 func test_initial_method_values():
 	assert_eq(antilock_braking.cycle_time(), 0.1, "Cycle time is 1/10.")
+
+func test_sensor_info_initial_values():
+	assert_eq(sensor_info.slip_ratio, 0.0, "Not slipping.")
+	assert_eq(sensor_info.input_brake_ratio, 0.0, "Not braking.")
+	assert_eq(sensor_info.abs_cycle, AntilockBraking.ABSCycle.PASS, "Doing nothing.")
+	assert_eq(sensor_info.output_brake_ratio, 0.0, "Not braking.")
+
+func test_value_clamping():
+	antilock_braking.target_slip = 1.0
+	assert_eq(antilock_braking.target_slip, 0.0, "Target slip can't be positive.")
+	antilock_braking.target_slip = -2.0
+	assert_eq(antilock_braking.target_slip, -1.0, "Target slip can't be below -1.")
+	
+	antilock_braking.cycle_time_left = 1.0
+	assert_almost_eq(antilock_braking.cycle_time_left, antilock_braking.cycle_time(), GConst.ERROR_INTERVAL, "Cycle time left can't be more than cycle time, wraps to cycle time.")
+	antilock_braking.cycle_time_left = -1.0
+	assert_almost_eq(antilock_braking.cycle_time_left, 0.0, GConst.ERROR_INTERVAL, "Cycle time left can't be negative, wraps to 0.")
+	
+	# SensorInfo
+	sensor_info.slip_ratio = 1.0
+	assert_eq(sensor_info.slip_ratio, 0.0, "Slip ratio can't be positive.")
+	sensor_info.slip_ratio = -2.0
+	assert_eq(sensor_info.slip_ratio, -1.0, "Slip ratio can't be below -1.")
+	
+	sensor_info.input_brake_ratio = 2.0
+	assert_eq(sensor_info.input_brake_ratio, 1.0, "Input brake ratio can't be above 1.")
+	sensor_info.input_brake_ratio = -1.0
+	assert_eq(sensor_info.input_brake_ratio, 0.0, "Input brake ratio can't be negative.")
+	
+	sensor_info.output_brake_ratio = 2.0
+	assert_eq(sensor_info.output_brake_ratio, 1.0, "Output brake ratio can't be above 1.")
+	sensor_info.output_brake_ratio = -1.0
+	assert_eq(sensor_info.output_brake_ratio, 0.0, "Output brake ratio can't be negative.")
+
+func test_cycle_time_left() -> void:
+	antilock_braking.simulate_abs(0.015625)
+	assert_eq(antilock_braking.cycle_time_left, 0.084375, "Cycle time left becomes 0.084375.")
 #endregion tests
