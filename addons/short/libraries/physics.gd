058d9b762d4fd52a4b0d35ffb5e41fe324a44ddc
tomsterBG <tomi4443321@gmail.com>
2026-08-23 09:45:22 +0300

    Add Physics earth_air_pressure, EARTH_AIR_TEMPERATURE_LAPSE_RATE, DRY_AIR_MOLAR_MASS, GAS_CONSTANT

--- addons/short/libraries/physics.gd
+++ addons/short/libraries/physics.gd
@@ -42,6 +42,7 @@
 ## Work with physics.
 ##
 ## Available in all scripts without any setup.
+##[br][br][b]Note:[/b] This assumes that the [Convert] class exists.
 
 @abstract class_name Physics extends Object
 
@@ -50,8 +51,14 @@
 ## The newtonian gravity constant in [code](N * m^2)/(kg^2)[/code]. See [url=https://en.wikipedia.org/wiki/Gravitational_constant]Gravitational constant[/url].
 const GRAVITY_CONSTANT := 6.6743e-11
 
+## The gas constant in [code]J * (mol * K)[/code]. See [url=https://en.wikipedia.org/wiki/Gas_constant]Gas constant[/url].
+const GAS_CONSTANT := 8.314_462_618_153_24
+
+## Dry air molar mass in [code]kg/mol[/code]. See [url=https://en.wikipedia.org/wiki/Atmosphere_of_Earth]Atmosphere of Earth[/url].
+const DRY_AIR_MOLAR_MASS := 0.028_946
+
 ## Earth's surface gravity in [code]meters/second^2[/code]. According to Wikipedia, gravity varies by 0.7% depending on your location.
-const EARTH_GRAVITY := 9.80665
+const EARTH_GRAVITY := 9.806_65
 
 ## Earth's radius in meters. According to Wikipedia, the radius varies by 0.3% depending on your distance from the equator.
 const EARTH_RADIUS := 6_371_000.0
@@ -59,8 +66,11 @@ const EARTH_RADIUS := 6_371_000.0
 ## Earth's mass in kg.
 const EARTH_MASS := 5.972e24
 
-## Earth's sea level air pressure in [code]atmospheres[/code].
+## Earth's sea level air pressure in [code]atmospheres[/code]. This assumes a temperature of 15°C.
 const EARTH_AIR_PRESSURE := 1.0
+
+## Earth's temperature lapse rate per meter in the standard troposphere in [code]kelvin/meter[/code]. See [url=https://en.wikipedia.org/wiki/Lapse_rate]Lapse rate[/url].
+const EARTH_AIR_TEMPERATURE_LAPSE_RATE := 0.0065
 
 ## Mars' surface gravity in [code]meters/second^2[/code].
 const MARS_GRAVITY := 3.728
@@ -78,4 +88,10 @@ static func gravity_between(mass_1_kg: float, mass_2_kg: float, distance_m: floa
 ## Returns gravity in [code]meters/second^2[/code] at [param height_m] from sea level in [code]meters[/code]. Ignores spin.
 static func earth_gravity(height_m: float) -> float:
 	return EARTH_GRAVITY * pow((EARTH_RADIUS) / (EARTH_RADIUS + height_m), 2)
+
+## Returns air pressure in [code]atmospheres[/code] at [param height_m] from sea level in [code]meters[/code] and [param temperature_c] at sea level in [code]celsius[/code].
+static func earth_air_pressure(height_m: float, temperature_c: float) -> float:
+	return EARTH_AIR_PRESSURE * pow(
+		1 - EARTH_AIR_TEMPERATURE_LAPSE_RATE * height_m / Convert.c_to_k(temperature_c),
+		EARTH_GRAVITY * DRY_AIR_MOLAR_MASS / (GAS_CONSTANT * EARTH_AIR_TEMPERATURE_LAPSE_RATE))
 #endregion methods
--- tests/accurate/test_physics.gd
+++ tests/accurate/test_physics.gd
@@ -4,10 +4,13 @@ extends GutTest
 #region tests
 func test_constants():
 	assert_eq(Physics.GRAVITY_CONSTANT, 6.6743e-11, "6.6743e-11 ((N * m^2)/(kg^2))")
+	assert_eq(Physics.GAS_CONSTANT, 8.314_462_618_153_24, "8.314_462_618_153_24 (J * (mol * K))")
+	assert_eq(Physics.DRY_AIR_MOLAR_MASS, 0.028_946, "0.028_946 (kg/mol)")
 	assert_eq(Physics.EARTH_GRAVITY, 9.80665, "Gravity (Earth) = 9.80665 (m/s^2)")
 	assert_eq(Physics.EARTH_RADIUS, 6371_000.0, "Radius (Earth) = 6371_000 (m)")
 	assert_eq(Physics.EARTH_MASS, 5.972e24, "Mass (Earth) = 5.972e24 (kg)")
 	assert_eq(Physics.EARTH_AIR_PRESSURE, 1.0, "Pressure (Earth) = 1 (atm)")
+	assert_eq(Physics.EARTH_AIR_TEMPERATURE_LAPSE_RATE, 0.0065, "Temperature (Earth) = 0.0065 (K/m)")
 	assert_eq(Physics.MARS_GRAVITY, 3.728, "Gravity (Mars) = 3.728 (m/s^2)")
 	assert_eq(Physics.MARS_MASS, 6.41693e23, "Mass (Mars) = 6.41693e23 (kg)")
 
@@ -17,4 +20,9 @@ func test_gravity_between():
 func test_earth_gravity():
 	assert_eq(Physics.earth_gravity(0.0), Physics.EARTH_GRAVITY, "Gravity at sea level.")
 	assert_almost_eq(Physics.earth_gravity(1000_000.0), 7.326_272, GConst.ERROR_INTERVAL, "Gravity at 1000 km.")
+
+func test_earth_air_pressure():
+	assert_eq(Physics.earth_air_pressure(0.0, 15.0), 1.0, "Standard atmosphere assumes 15°C.")
+	assert_almost_eq(Physics.earth_air_pressure(5_000.0, 15.0), 0.533_353, GConst.ERROR_INTERVAL, "At 5km summit.")
+	assert_almost_eq(Physics.earth_air_pressure(11_000.0, 15.0), 0.223_579, GConst.ERROR_INTERVAL, "At 11km jet cruise.")
 #endregion tests
