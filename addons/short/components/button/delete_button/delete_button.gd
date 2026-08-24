3aa0e2ccdb19e140e414d470ed38465413ffd79b
tomsterBG <tomi4443321@gmail.com>
2026-08-24 18:54:55 +0300

    Add DeleteButton

---
+++ addons/short/components/button/delete_button/delete_button.gd
@@ -0,0 +1,68 @@
+# TODO:
+# IDEAS:
+# - x icon for the node
+
+## A button that deletes a [Node].
+##
+## When the button is pressed, the [member delete_this_node] will be deleted.
+##[br][br][b]Note:[/b] This overrides [method Node._get_configuration_warnings] and [method BaseButton._pressed]. Use [code]super()[/code] if you want to extend the same methods.
+
+@tool
+class_name DeleteButton extends Button
+
+
+#region enums
+## A method to use for deletion.
+enum DeletionMethod {
+	## Use [method Node.free].
+	FREE,
+	## Use [method Node.queue_free].
+	QUEUE_FREE,
+}
+#endregion enums
+
+
+#region variables
+@export_group("Delete", "delete_")
+
+## Toggles whether to trigger deletion when the button is pressed.
+@export var delete_on_pressed: bool = false
+
+## Deletes this [Node] when the button is pressed.
+@export var delete_this_node: Node: set = set_delete_this_node
+
+## Uses this method for deletion.
+@export var delete_method: DeletionMethod = DeletionMethod.QUEUE_FREE
+#endregion variables
+
+
+#region setters
+func set_delete_this_node(value: Node) -> void:
+	delete_this_node = value
+	if Engine.is_editor_hint(): update_configuration_warnings()
+#endregion setters
+
+
+#region methods
+## Deletes [member delete_this_node] according to [member delete_method].
+func delete_node() -> void:
+	if !delete_this_node: return
+	match delete_method:
+		DeletionMethod.FREE:
+			delete_this_node.free()
+		DeletionMethod.QUEUE_FREE:
+			delete_this_node.queue_free()
+#endregion methods
+
+
+#region virtual
+func _pressed() -> void:
+	if !delete_on_pressed: return
+	delete_node()
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray
+	if !delete_this_node:
+		warnings.append("Missing node to delete.")
+	return warnings
+#endregion virtual
---
+++ tests/accurate/button/test_delete_button.gd
@@ -0,0 +1,25 @@
+extends GutTest
+
+
+#region variables
+var delete_button: DeleteButton
+#endregion variables
+
+
+#region virtual
+func before_each():
+	delete_button = DeleteButton.new()
+	add_child_autofree(delete_button)
+	watch_signals(delete_button)
+#endregion virtual
+
+
+#region tests
+func test_initial_values():
+	assert_false(delete_button.delete_on_pressed, "Delete on pressed is disabled.")
+	assert_null(delete_button.delete_this_node, "Node is null.")
+	assert_eq(delete_button.delete_method, DeleteButton.DeletionMethod.QUEUE_FREE, "Method is queue free.")
+
+func test_initial_method_values():
+	assert_eq(delete_button._get_configuration_warnings().size(), 1, "Node is null.")
+#endregion tests
